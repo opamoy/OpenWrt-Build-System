8296807c7e0ca913e266b4b6349c9c9935d9aaa0
opamoy <amoyxsat+op@gmail.com>
2025-03-29 16:40:18 +0800

    脚本文件的行尾符号转换为 Unix 格式

--- customize/lede_x86_64.sh
+++ customize/lede_x86_64.sh
@@ -1,101 +1,101 @@
 #!/bin/bash
 #=================================================
 # 自定义脚本修复版
+# 强制锁定工作目录到项目根目录
 #=================================================
-##########################################添加额外包##########################################
+cd "$(dirname "$0")/.." || { echo "无法进入项目目录"; exit 1; }
 
-# Git稀疏克隆函数修复
+########################################## 添加额外包 ##########################################
 function git_sparse_clone() {
- branch="$1" repourl="$2" && shift 2
- git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
- repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
- cd $repodir && git sparse-checkout set $@
- mv -f $@ package/linpc  # 修复路径：移除 "../"
- cd .. && rm -rf $repodir
+  branch="$1" repourl="$2" && shift 2
+  echo "正在克隆仓库: $repourl (分支: $branch)"
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repouru || { echo "克隆失败: $repourl"; exit 1; }
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd "$repodir" || exit 1
+  git sparse-checkout set "$@" || { echo "设置稀疏检出失败"; exit 1; }
+  mkdir -p ../package/linpc
+  mv -f "$@" ../package/linpc || { echo "移动文件失败"; ls -l; exit 1; }
+  cd .. && rm -rf "$repodir"
 }
 
-# 创建目录
+# 创建目录并清理冲突包
 mkdir -p package/linpc
-
-# 移除冲突包
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
 
-#luci-theme-argone
-git_sparse_clone main https://github.com/kenzok8/small-package luci-theme-argone
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-argone-config
+# luci-theme-argone
+git_sparse_clone main https://github.com/kenzok8/small-package luci-theme-argone luci-app-argone-config
 
-#luci-app-store依赖
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-lib-taskd
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-lib-xterm
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages taskd
+# luci-app-store 依赖
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-lib-taskd luci-lib-xterm taskd
 
-#科学上网插件
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-openclash
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-passwall
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ssr-plus
+# 科学上网插件
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-openclash luci-app-passwall luci-app-ssr-plus
 
-#Netdata
+# Netdata
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/linpc/luci-app-netdata
-sed -i 's/"status"/"system"/g' package/linpc/luci-app-netdata/luasrc/controller/*.lua
-sed -i 's/"status"/"system"/g' package/linpc/luci-app-netdata/luasrc/model/cgi/*.lua
-sed -i 's/admin\/status/admin\/system/g' package/linpc/luci-app-netdata/luasrc/view/netdata/*.htm
+if [ -d "package/linpc/luci-app-netdata" ]; then
+  sed -i 's/"status"/"system"/g' package/linpc/luci-app-netdata/luasrc/controller/*.lua
+  sed -i 's/"status"/"system"/g' package/linpc/luci-app-netdata/luasrc/model/cgi/*.lua
+  sed -i 's/admin\/status/admin\/system/g' package/linpc/luci-app-netdata/luasrc/view/netdata/*.htm
+else
+  echo "警告: luci-app-netdata 未成功克隆"
+fi
 
-#mosdns
-git_sparse_clone v5 https://github.com/sbwml/luci-app-mosdns luci-app-mosdns
-git_sparse_clone v5 https://github.com/sbwml/luci-app-mosdns mosdns
+# Mosdns
+git_sparse_clone v5 https://github.com/sbwml/luci-app-mosdns luci-app-mosdns mosdns
 rm -rf feeds/packages/utils/v2dat
 rm -rf package/feeds/packages/v2dat
 git_sparse_clone v5 https://github.com/sbwml/luci-app-mosdns v2dat
 
-#luci-app-autotimeset
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-autotimeset
-
-########依赖包########
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages brook
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages chinadns-ng
-# ... 其他依赖包克隆保持不变 ...
-
-##########################################其他设置##########################################
-
+########################################## 系统设置 ##########################################
 # 修改默认登录地址
-sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
+CONFIG_GENERATE="package/base-files/files/bin/config_generate"
+if [ -f "$CONFIG_GENERATE" ]; then
+  sed -i 's/192.168.1.1/192.168.99.1/g' "$CONFIG_GENERATE"
+else
+  echo "错误: $CONFIG_GENERATE 不存在"
+  exit 1
+fi
 
-# 修改默认登录密码
-sed -i 's/$1$eRZDGn.w$lAHe0nuYvaem61CpArhxV.//g' package/lean/default-settings/files/zzz-default-settings
+# 修改默认密码
+ZZZ_SETTINGS="package/lean/default-settings/files/zzz-default-settings"
+if [ -f "$ZZZ_SETTINGS" ]; then
+  sed -i 's/$1$eRZDGn.w$lAHe0nuYvaem61CpArhxV.//g' "$ZZZ_SETTINGS"
+else
+  echo "警告: $ZZZ_SETTINGS 不存在"
+fi
 
 # TTYD 免登录
-sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+TTYD_CONFIG="feeds/packages/utils/ttyd/files/ttyd.config"
+if [ -f "$TTYD_CONFIG" ]; then
+  sed -i 's|/bin/login|/bin/login -f root|g' "$TTYD_CONFIG"
+else
+  echo "警告: $TTYD_CONFIG 未找到"
+fi
 
-# 添加项目地址
-sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a <tr><td width="33%"><%:Github项目%><\/td><td><a href="https:\/\/github.com\/rnamoy\/OpenWrt-Build-System" target="_blank">Discuzamoy<\/a><\/td><\/tr>' package/lean/autocore/files/x86/index.htm
-
-# 修改本地时间格式
-sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
+# 添加项目地址到状态页
+INDEX_HTML="package/lean/autocore/files/x86/index.htm"
+if [ -f "$INDEX_HTML" ]; then
+  sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a <tr><td width="33%"><%:Github项目%><\/td><td><a href="https:\/\/github.com\/rnamoy\/OpenWrt-Build-System" target="_blank">Discuzamoy<\/a><\/td><\/tr>' "$INDEX_HTML"
+else
+  echo "警告: $INDEX_HTML 未找到"
+fi
 
 # 修改镜像源
-sed -i 's#mirror.iscas.ac.cn/kernel.org#mirrors.edge.kernel.org/pub#' scripts/download.pl
+DOWNLOAD_PL="scripts/download.pl"
+if [ -f "$DOWNLOAD_PL" ]; then
+  sed -i 's#mirror.iscas.ac.cn/kernel.org#mirrors.edge.kernel.org/pub#' "$DOWNLOAD_PL"
+else
+  echo "错误: $DOWNLOAD_PL 不存在"
+  exit 1
+fi
 
-# 修改版本为编译日期
-date_version=$(date +"%y.%m.%d")
-orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by Linpc/g" package/lean/default-settings/files/zzz-default-settings
-
-# 删除无效opkg源
-sed -i '/exit 0/i sed -i "/kiddin9/d" /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
-sed -i '/exit 0/i sed -i "/kenzo/d" /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
-sed -i '/exit 0/i sed -i "/small/d" /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
-
-# 删除多余文件
-sed -i '/exit 0/i\rm -f /etc/config/adguardhome\nrm -f /etc/init.d/adguardhome' package/lean/default-settings/files/zzz-default-settings
-
-# 修复Makefile路径
-find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
-find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
-
-# 修复golang版本
+# 其他修复
 mkdir -p feeds/packages/lang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-chmod -R 777 feeds/packages/lang/golang
+chmod -R 755 feeds/packages/lang/golang
+
+echo "所有自定义操作已完成"
